a90f6e6160f860c6df810d26e72d9ef5287d6c04
Mosh Hamedani <programmingwithmosh#gmail.com>
2023-03-30 09:57:49 +0530

    first init

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import "./Navbarstyle.css"
+
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <div className="header">
+            <Link to="/">
+            <h1>Portfolio</h1>
+            </Link>
+        </div>
+    )
+}
