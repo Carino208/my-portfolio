87c2153b1dd4ac19292e4d88327ad8e606183eed
Mosh Hamedani <programmingwithmosh#gmail.com>
2023-04-23 01:26:36 +0530

    Added Pricing Card

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,15 +10,14 @@ const Footer = () => {
         <div className="footer-container">
           <div className="left">
             <div className="location">
-                <h4><FaHome size={20} style={{ color: "#fff", marginRight: "2rem"}} />
-                </h4>
+                <FaHome size={30} style={{ color: "#fff", marginRight: "2rem"}} />
                 <div>
                   <p>168,Amalapuram.</p>
                   <p>Andhra Pradesh.</p>
                 </div> 
                 <div className="phone">
                 <h4><FaPhone size={20} style={{ color: "#fff", marginRight: "2rem"}} />
-                955-64-434</h4>
+                   9550640434</h4>
                 </div>
                 <div className="email">
                 <h4><FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem"}} />
@@ -32,26 +31,25 @@ const Footer = () => {
             <p>This is me Harish Panthadi.As a web developer, I have many years of experience with CSS, JavaScript, and HTML.I enjoy learning new things.</p>
             <div className="social">
               <FaFacebook
-                      size={20}
+                      size={30}
                       style={{ color: "#fff",
                        marginRight: "1rem" }}
               />
                <FaTwitter
-                      size={20}
+                      size={30}
                       style={{ color: "#fff",
                        marginRight: "1rem" }}
               />
                <FaLinkedin
-                      size={20}
+                      size={30}
                       style={{ color: "#fff",
                        marginRight: "1rem" }}
               />
             </div>
-
           </div>  
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer
